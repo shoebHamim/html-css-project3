54fe17853a1b3b67b805808a40cd7d918d344602
shoebHamim <hamim99bd@gmail.com>
2022-07-11 14:06:39 +0600

    all done

---
+++ app.js
@@ -0,0 +1,8 @@
+const nav=document.querySelector('.nav-links');
+const burger=document.querySelector('.burger');
+const links= nav.querySelectorAll('a');
+
+burger.addEventListener('click',()=>{
+    nav.classList.toggle('nav-open');
+    burger.classList.toggle('toggle')
+})
